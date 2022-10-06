fda9d9b7fb4a281bd41ca276483d202e93618474
PedrohDavi <pedro.henrique04896@gmail.com>
2022-10-06 19:38:27 -0300

    inicio

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
-from re import template
-from tkinter import Button
 from flask import Flask, render_template
+
 app = Flask("__name__")
 
-@app.route("/")
+@app.route ("/")
 @app.route("/home")
 def home ():
     return render_template("home.html")
-
-
-@app.route("/jogos")
-def quemsomos():
-    return render_template("jogos.html")
-    
-
-@app.route("/celulares")
-def contato():
-    return render_template("celulares.html")
-
-    @app.route("/jogos")
-def quemsomos():
-    return render_template("jogos.html")
-    
-
-@app.route("/celulares")
-def contato():
-    return render_template("celulares.html")
 
 if __name__=='__main__':
     app.run(debug=True)
